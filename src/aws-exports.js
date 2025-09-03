3fac2b62d165f479336ea85dd1cdfd918eb2a40f
simple.is.best0706@gmail.com <simple.is.best0706@gmail.com>
2025-09-03 09:57:29 +0900

    困ったらここに戻る

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,7 @@ const config =  {
           endpoint: 'https://lnbqw6f7avh7ze3szmlszlhrzy.appsync-api.ap-northeast-1.amazonaws.com/graphql',
           region: 'ap-northeast-1',
           defaultAuthMode: 'apiKey',
-          apiKey: 'da2-vwj5tpjmgfak7fqnot5c3ndkvq'
+          apiKey: 'da2-vr3po2phkzdzta27qjppkdggqm'
         }
     }
 };
