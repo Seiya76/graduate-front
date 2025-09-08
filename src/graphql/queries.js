85200a7c8363ac66cc7e6fa79e95b6bf5d2a9888
simple.is.best0706@gmail.com <simple.is.best0706@gmail.com>
2025-09-08 13:40:56 +0900

    a

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,87 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getUserRooms = /* GraphQL */ `
+  query GetUserRooms($userId: ID!, $limit: Int, $nextToken: String) {
+    getUserRooms(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      items {
+        roomId
+        roomName
+        roomType
+        createdBy
+        createdAt
+        lastMessage
+        lastMessageAt
+        memberCount
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getRoom = /* GraphQL */ `
+  query GetRoom($roomId: ID!) {
+    getRoom(roomId: $roomId) {
+      roomId
+      roomName
+      roomType
+      createdBy
+      createdAt
+      lastMessage
+      lastMessageAt
+      memberCount
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const getCurrentUser = /* GraphQL */ `
+  query GetCurrentUser {
+    getCurrentUser {
+      userId
+      createdAt
+      email
+      emailVerified
+      nickname
+      status
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const getUser = /* GraphQL */ `
+  query GetUser($userId: ID!) {
+    getUser(userId: $userId) {
+      userId
+      createdAt
+      email
+      emailVerified
+      nickname
+      status
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const searchUsers = /* GraphQL */ `
+  query SearchUsers($searchTerm: String!, $limit: Int) {
+    searchUsers(searchTerm: $searchTerm, limit: $limit) {
+      items {
+        userId
+        createdAt
+        email
+        emailVerified
+        nickname
+        status
+        updatedAt
+        __typename
+      }
+      __typename
+    }
+  }
+`;
 export const getRoomMessages = /* GraphQL */ `
   query GetRoomMessages(
     $roomId: ID!
@@ -79,87 +160,6 @@ export const getUserMessages = /* GraphQL */ `
       }
       nextToken
       hasMore
-      __typename
-    }
-  }
-`;
-export const getUserRooms = /* GraphQL */ `
-  query GetUserRooms($userId: ID!, $limit: Int, $nextToken: String) {
-    getUserRooms(userId: $userId, limit: $limit, nextToken: $nextToken) {
-      items {
-        roomId
-        roomName
-        roomType
-        createdBy
-        createdAt
-        lastMessage
-        lastMessageAt
-        memberCount
-        updatedAt
-        __typename
-      }
-      nextToken
-      __typename
-    }
-  }
-`;
-export const getRoom = /* GraphQL */ `
-  query GetRoom($roomId: ID!) {
-    getRoom(roomId: $roomId) {
-      roomId
-      roomName
-      roomType
-      createdBy
-      createdAt
-      lastMessage
-      lastMessageAt
-      memberCount
-      updatedAt
-      __typename
-    }
-  }
-`;
-export const getCurrentUser = /* GraphQL */ `
-  query GetCurrentUser {
-    getCurrentUser {
-      userId
-      createdAt
-      email
-      emailVerified
-      nickname
-      status
-      updatedAt
-      __typename
-    }
-  }
-`;
-export const getUser = /* GraphQL */ `
-  query GetUser($userId: ID!) {
-    getUser(userId: $userId) {
-      userId
-      createdAt
-      email
-      emailVerified
-      nickname
-      status
-      updatedAt
-      __typename
-    }
-  }
-`;
-export const searchUsers = /* GraphQL */ `
-  query SearchUsers($searchTerm: String!, $limit: Int) {
-    searchUsers(searchTerm: $searchTerm, limit: $limit) {
-      items {
-        userId
-        createdAt
-        email
-        emailVerified
-        nickname
-        status
-        updatedAt
-        __typename
-      }
       __typename
     }
   }
