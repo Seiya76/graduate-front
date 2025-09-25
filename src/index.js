eb77467119955beaee3785c8c61da1c6f820d373
simple.is.best0706@gmail.com <simple.is.best0706@gmail.com>
2025-09-25 12:41:40 +0900

    a

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import config from './aws-exports'; //Appsync用のConfigをインポート
 Amplify.configure(config);
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_ncffAodBj",
+  authority: "https://ap-northeast-1ncffaodbj.auth.ap-northeast-1.amazoncognito.com",
   client_id: "5buno8gs9brj93apmu9tvqqp77",
   redirect_uri: "https://main.d3rgq9lalaa9gb.amplifyapp.com",
   post_logout_redirect_uri: "https://main.d3rgq9lalaa9gb.amplifyapp.com",
   response_type: "code",
   scope: "openid email",
-  storage: window.localStorage, // ← ここ大事！リロードしても state が消えない
+  storage: window.localStorage, 
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -28,8 +28,3 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
